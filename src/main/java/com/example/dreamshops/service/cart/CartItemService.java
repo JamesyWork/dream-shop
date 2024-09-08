c8d5441c6518a173f490c045f2790a402b1ca343
JamesyWork <133091941+JamesyWork@users.noreply.github.com>
2024-09-09 01:25:23 +0800

    feat: done implement cart

---
+++ src/main/java/com/example/dreamshops/service/cart/CartItemService.java
@@ -0,0 +1,78 @@
+package com.example.dreamshops.service.cart;
+
+import com.example.dreamshops.exception.ResourceNotFoundException;
+import com.example.dreamshops.model.Cart;
+import com.example.dreamshops.model.CartItem;
+import com.example.dreamshops.model.Product;
+import com.example.dreamshops.repository.CartItemRepository;
+import com.example.dreamshops.repository.CartRepository;
+import com.example.dreamshops.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class CartItemService implements ICartItemService {
+    private final CartItemRepository cartItemRepository;
+    private final CartRepository cartRepository;
+    private final ICartService cartService;
+    private final IProductService productService;
+
+    @Override
+    public void addItemToCart(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        Product product = productService.getProductById(productId);
+        CartItem cartItem = cart.getCartItems()
+                .stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst().orElse(new CartItem());
+        if (cartItem.getId() == null) {
+            cartItem.setCart(cart);
+            cartItem.setProduct(product);
+            cartItem.setQuantity(quantity);
+            cartItem.setUnitPrice(product.getPrice());
+        }
+        else {
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+        cartItem.setTotalPrice();
+        cart.addCartItem(cartItem);
+        cartItemRepository.save(cartItem);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void removeItemFromCart(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        CartItem itemToRemove = getCartItem(cartId, productId);
+        cart.removeCartItem(itemToRemove);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public void updateItemQuantity(Long cartId, Long productId, int quantity) {
+        Cart cart = cartService.getCart(cartId);
+        cart.getCartItems().stream().filter(
+                item -> item.getProduct().getId().equals(productId)
+        ).findFirst().ifPresent(item -> {
+            item.setQuantity(quantity);
+            item.setUnitPrice(item.getProduct().getPrice());
+            item.setTotalPrice();
+        });
+        BigDecimal totalAmount = cart.getCartItems()
+                .stream().map(CartItem::getTotalPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        cart.setTotalAmount(totalAmount);
+        cartRepository.save(cart);
+    }
+
+    @Override
+    public CartItem getCartItem(Long cartId, Long productId) {
+        Cart cart = cartService.getCart(cartId);
+        return cart.getCartItems().stream().filter(
+                item -> item.getProduct().getId().equals(productId)
+        ).findFirst().orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+    }
+}
