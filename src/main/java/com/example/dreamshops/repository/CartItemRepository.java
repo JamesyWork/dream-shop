5f7358a200941802554766eca4a51877d542e104
JamesyWork <133091941+JamesyWork@users.noreply.github.com>
2024-09-09 01:13:04 +0800

    Revert "Feat/cart (#5)"
    This reverts commit 3782bba63404fd5148c9da8c8f7c93d3580bdba1.

--- src/main/java/com/example/dreamshops/repository/CartItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.dreamshops.repository;
-
-import com.example.dreamshops.model.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    void deleteAllByCartId(Long id);
-}
